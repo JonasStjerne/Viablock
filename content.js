25edf6267a185bdacd5b197e9161b7f208ceadf2
Jonas Stjerne <73853586+JonasStjerne@users.noreply.github.com>
2021-10-31 17:40:57 +0100

    Bugfix: New selector

--- content.js
+++ content.js
@@ -1,5 +1,6 @@
 setInterval(function () {
     check();
+    console.log(document.querySelectorAll('.e3bxrhk0')[0]?.innerHTML.replace(/\D/g,''));
 }, 2000);
 var playedToRecorded = 0;
 
@@ -7,12 +8,15 @@ function check() {
     var lastVideoProgress = JSON.parse(localStorage.lastVideoProgress);
     if (Number(lastVideoProgress.playedTo)) {
          playedToRecorded  = Number(lastVideoProgress.playedTo);
+         console.log(playedToRecorded);
     }
 
-    if (document.querySelectorAll('video.vjs-tech')[0].src && !document.querySelectorAll('video.vjs-tech')[0].src.startsWith("blob:") && document.querySelectorAll('span.ee5kzpm1')[0]?.innerHTML.replace(/\D/g,'') > 5) {
-        playedToRecorded += 1;
+    if (document.querySelectorAll('video.vjs-tech')[0].src && !document.querySelectorAll('video.vjs-tech')[0].src.startsWith("blob:") && document.querySelectorAll('.e3bxrhk0')[0]?.innerHTML.replace(/\D/g,'') > 60) {
+        playedToRecorded += 5;
+        console.log("Played to +2:" + playedToRecorded);
         lastVideoProgress.playedTo = playedToRecorded;
         localStorage.lastVideoProgress = JSON.stringify(lastVideoProgress);
         location.reload();
     }
 }
+// && document.querySelectorAll('.eihsxth0')[0]?.innerHTML.replace(/\D/g,'') > 5
